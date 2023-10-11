3276908cfee4194c0ee6a6325cd4e243db02b205
Magomedcoder <info@magomedcoder.ru>
2023-10-11 12:21:39 +0300

    first commit

---
+++ SimpleJwt/SimpleJwtApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleJwtApp.swift
+//  SimpleJwt
+//
+//  Created by Magomedcoder on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleJwtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
